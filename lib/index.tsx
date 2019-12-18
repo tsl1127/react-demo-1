9033aab1dbdbd0ad75501d089a2aab74f5b05e54
tsl <taosiliang1127tsl@163.com>
2019-12-18 15:12:08 +0800

    icon组件FunctionComponent

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,8 +1,12 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom'
-import Button from './button'
+import React from 'react';
+import ReactDOM from 'react-dom'
+// import Button from './button'
+import Icon from './icon'
 
-console.log('hi')
+// console.log('hi')
 // ReactDOM.render(<Button></Button>,document.body)
-const a = 1
-export {Button, a}
+ReactDOM.render(<div>
+    <Icon name="wechat"></Icon>
+</div>,document.body)
+// const a = 1
+// export {Button, a}
